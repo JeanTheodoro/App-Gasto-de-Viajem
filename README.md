09c806a873d0bf6f5dd779f2482454da64ca7541
JeanTheodoro <jean.theodorosilva@gmail.com>
2020-08-21 19:44:36 -0300

    implemnetação do calculo

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-"# App-Gasto-de-Viajem" 
+"# App-Gasto-de-Viajem"
+
+# App Notas de Tarefas
+
+## 📷 Demonstração
+
+
+_______________
+## 💻 Atividade desenvolvida:
+
+* Desenvolvimento de um  Acitivity (container: _LinearLayout_) principal, que lê os dados e realiza o cálculo de custo.
+
+_______________
+## 🚀 Configurações
+
+* Android Studio versâo 4.0
+* Api 24 android _Nougat_
+* Linguagem _Kotlin_
+
+
--- app/src/main/java/br/app/gatosviagem/com/MainActivity.kt
+++ app/src/main/java/br/app/gatosviagem/com/MainActivity.kt
@@ -2,10 +2,59 @@ package br.app.gatosviagem.com
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+
+        val id = view?.id
+
+        if (id == R.id.buttonCalculate) {
+
+            calculate()
+
+        }
+    }
+
+    private fun calculate() {
+
+        if (validationOk()) {
+
+            try {
+                val distance = editetxDistance.text.toString().toFloat()
+                val price = edittextPrice.text.toString().toFloat()
+                val litro = edittextLitro.text.toString().toFloat()
+
+                val totalValue = (distance * price) / litro
+                textviewValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+            } catch (erro: NumberFormatException) {
+
+                Toast.makeText(applicationContext, R.string.menssage_erro_value, Toast.LENGTH_LONG)
+                    .show()
+            }
+
+        } else {
+
+            Toast.makeText(applicationContext, R.string.menssage_erro, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun validationOk(): Boolean {
+
+        return (!editetxDistance.text.toString().isEmpty() &&
+                !edittextPrice.text.toString().isEmpty() &&
+                !edittextLitro.text.toString().isEmpty())
+
     }
 }
